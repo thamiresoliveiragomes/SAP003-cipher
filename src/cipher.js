88587eed1fd1f556f9837722b7374135c842a52e
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2019-08-23 19:12:18 -0300

    passou nos testes

--- src/cipher.js
+++ src/cipher.js
@@ -2,22 +2,21 @@ window.cipher = {
   encode: encode,
   decode: decode
 };
-function encode(offset, string){
+function encode(offset, string) {
   let arrayResposta = [];
   let tamanho = string.length;
-  let array = Array.from(string);
-  for(let i=0; i<tamanho; i++){
-    let stringToAscii = array[i].charCodeAt(0);
-    if(stringToAscii >= 65 && stringToAscii <= 90){
+  for (let i=0; i<tamanho; i++) {
+    let stringToAscii = string[i].charCodeAt(0);
+    if (stringToAscii >= 65 && stringToAscii <= 90) {
       let calculo = (stringToAscii - 65 + offset) % 26 + 65;
-      let result = String.fromCharCode(calculo);
-      arrayResposta.push(result);
-    }else if(stringToAscii >= 97 && stringToAscii <= 122){
+      let result = String.fromCharCode (calculo);
+      arrayResposta.push (result);
+    } else if (stringToAscii >= 97 && stringToAscii <= 122) {
       let calculo = (stringToAscii - 97 + offset) % 26 + 97;
       let result = String.fromCharCode(calculo);
       arrayResposta.push(result);
-    }else{
-      arrayResposta.push(array[i]);
+    } else {
+      arrayResposta.push(string[i]);
     }
   }
   return arrayResposta.join("");
@@ -25,30 +24,19 @@ function encode(offset, string){
 function decode(offset, string) {
   let arrayResposta = [];
   let tamanho = string.length;
-  let array = Array.from(string);
-  for(let i=0; i<tamanho; i++){
-    let stringToAscii = array[i].charCodeAt(0);
-    if(stringToAscii >= 65 && stringToAscii <= 90){
-      let formula = (stringToAscii - 65 - offset + 26);
-      while(formula<0){
-      formula = formula + 26;
-      }
-      let calculo = (formula)% 26 + 65;
+  for (let i=0; i<tamanho; i++) {
+    let stringToAscii = string[i].charCodeAt(0);
+    if (stringToAscii >= 65 && stringToAscii <= 90) {
+      let calculo = (stringToAscii - 90 - offset)% 26 + 90;
+      let result = String.fromCharCode (calculo);
+      arrayResposta.push (result);
+    } else if (stringToAscii >= 97 && stringToAscii <= 122) {
+      let calculo = (stringToAscii - 122 - offset) % 26 + 122;
       let result = String.fromCharCode(calculo);
       arrayResposta.push(result);
-    }else if(stringToAscii >= 97 && stringToAscii <= 122){
-      let formula = (stringToAscii - 97 - offset + 26);
-      while(formula<0){
-      formula = formula + 26;
-      }
-      let calculo = (formula)% 26 + 97;
-      let result = String.fromCharCode(calculo);
-      arrayResposta.push(result);
-    }else{
-      arrayResposta.push(array[i]);
+    } else {
+      arrayResposta.push(string[i]);
     }
   }
   return arrayResposta.join("");
 }
-
- 
--- src/index.js
+++ src/index.js
@@ -1,60 +1,41 @@
-function queroCodificar(event){
-    event.preventDefault();
-    document.getElementById("inicio").classList.add("invisivel");
-    document.getElementById("queroCodificar").classList.remove("invisivel");
-}
-function queroDescodificar(event){
-    event.preventDefault();
-    document.getElementById("inicio").classList.add("invisivel");
-    document.getElementById("queroDescodificar").classList.remove("invisivel")
-}
-function voltar1(event){
-    event.preventDefault();
-    document.getElementById("inicio").classList.remove("invisivel");
-    document.getElementById("queroCodificar").classList.add("invisivel");
-}
-function voltar2(event){
-    event.preventDefault();
-    document.getElementById("inicio").classList.remove("invisivel");
-    document.getElementById("queroDescodificar").classList.add("invisivel");
-}
-function limpar1(event){
-    event.preventDefault();
-    document.getElementById("resultCodificar").classList.add("invisivel");
-    document.getElementById("mensagemCodificar").value = '';
-    document.getElementById("offset1").value = '';
-}
-function limpar2(event){
-    event.preventDefault();
+document.getElementById("codificar").addEventListener("click", codificar);
+document.getElementById("descodificar").addEventListener("click", descodificar);
+document.getElementById("limpar").addEventListener("click", limpar);
+
+function codificar(event) {
+  event.preventDefault();
+  let offset = Number(document.getElementById("offset").value);
+  let string = document.getElementById("mensagem").value;
+  if (string === "") {
+    alert("Preencha a mensagem a ser codificada");
+  } else if (offset <= 0 || !Number.isInteger(offset)) {
+    alert("Digite uma chave de deslocamento válida");
+  } else {
+    let result = window.cipher.encode(offset, string);
+    document.getElementById("resultCodificar").classList.remove("invisivel");
     document.getElementById("resultDescodificar").classList.add("invisivel");
-    document.getElementById("mensagemDescodificar").value = '';
-    document.getElementById("offset2").value = '';
+    document.getElementById("resultCodificar").innerHTML = `<p>A mensagem codificada é: <p>${result}</p></p>`;
+  }
 }
-function codificar(event){
-    event.preventDefault();
-    let offset = Number(document.getElementById("offset1").value);
-    let string = document.getElementById("mensagemCodificar").value;
-    if(string === ''){
-        alert("Preencha a mensagem a ser codificada");
-    }else if(offset <= 0){
-        alert("Preencha a chave de deslocamento com um número positivo");
-    }else{
-        let result = window.cipher.encode(offset, string);
-        document.getElementById("resultCodificar").classList.remove("invisivel");
-        document.getElementById("resultCodificar").innerHTML = `<p>A mensagem codificada é: <p>${result}</p></p>`;
-    }
+function descodificar(event) {
+  event.preventDefault();
+  let offset = Number(document.getElementById("offset").value);
+  let string = document.getElementById("mensagem").value;
+  if (string === "") {
+    alert("Preencha a mensagem a ser descodificada");
+  } else if (offset <= 0 || !Number.isInteger(offset)) {
+    alert("Digite uma chave de deslocamento válida");
+  } else {
+    let result = window.cipher.decode(offset, string);
+    document.getElementById("resultCodificar").classList.add("invisivel");
+    document.getElementById("resultDescodificar").classList.remove("invisivel");
+    document.getElementById("resultDescodificar").innerHTML = `<p>A mensagem descodificada é: <p>${result}</p></p>`;
+  }
 }
-function descodificar(event){
-    event.preventDefault();
-    let offset = Number(document.getElementById("offset2").value);
-    let string = document.getElementById("mensagemDescodificar").value;
-    if(string === ''){
-        alert("Preencha a mensagem a ser descodificada");
-    }else if(offset <= 0){
-        alert("Preencha a chave de deslocamento com um número positivo");
-    }else{
-        let result = window.cipher.decode(offset, string);
-        document.getElementById("resultDescodificar").classList.remove("invisivel");
-        document.getElementById("resultDescodificar").innerHTML = `<p>A mensagem descodificada é: <p>${result}</p></p>`;
-    }
+function limpar(event) {
+  event.preventDefault();
+  document.getElementById("resultCodificar").classList.add("invisivel");
+  document.getElementById("resultDescodificar").classList.add("invisivel");
+  document.getElementById("mensagem").value = "";
+  document.getElementById("offset").value = "";
 }
